eb2c9f8c5b599f497d1129bb82f5d4886390c431
HigashiHayato <higashi_hayato@ims-sol.co.jp>
2024-02-26 23:37:51 +0900

    render へのデプロイ調整

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Bootアプリケーションのエントリーポイントとなるクラスです.
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+	/**
+	 * アプリケーションのメインメソッドです。Spring Bootアプリケーションを起動します.
+	 *
+	 * @param args アプリケーション起動時に渡されるコマンドライン引数
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
