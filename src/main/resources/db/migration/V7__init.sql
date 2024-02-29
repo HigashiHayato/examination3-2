534fe5047b9389887a003145b506dcc664505681
HigashiHayato <higashi_hayato@ims-sol.co.jp>
2024-03-01 00:27:59 +0900

    静的解析における修正

---
+++ src/main/resources/db/migration/V7__init.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE books (
+    id VARCHAR(10) PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    author VARCHAR(50) NOT NULL,
+    publisher VARCHAR(50) NOT NULL,
+    price INT NOT NULL
+);
